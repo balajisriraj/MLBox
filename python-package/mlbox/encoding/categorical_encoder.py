347bd4c617902abab58f9705bfcfcdabb9d32b00
Axel <axelderomblay@gmail.com>
2017-07-04 09:53:24 +0200

    Update categorical_encoder.py

--- python-package/mlbox/encoding/categorical_encoder.py
+++ python-package/mlbox/encoding/categorical_encoder.py
@@ -371,7 +371,7 @@ class Categorical_encoder():
                         unique_levels = set(df[col].values)
                         sub_levels = unique_levels & set(self.__Enc[col])
                         missing_levels = [col+"_"+ str(s) for s in list(set(self.__Enc[col]) - sub_levels)]
-                        sub_levels = [col+"_"+ s for str(s) in list(sub_levels)]
+                        sub_levels = [col+"_"+ str(s) for s in list(sub_levels)]
 
                         sub_var = sub_var + sub_levels
                         missing_var = missing_var + missing_levels
