3d6968f8304290eb392aea1d580caed510b94203
Axel <axelderomblay@gmail.com>
2017-07-12 11:11:55 +0200

    Add files via upload

--- python-package/setup.py
+++ python-package/setup.py
@@ -4,17 +4,27 @@
 import pip
 from setuptools import setup
 from setuptools.command.install import install
-from pip.req import parse_requirements
 
-install_reqs = parse_requirements("./requirements.txt", session=False)
-reqs = [str(ir.req) for ir in install_reqs]
+requirements = [
+    "numpy>=1.13.0",
+    "matplotlib>=2.0.2",
+    "hyperopt==0.1",
+    "ipyparallel==6.0.0",
+    "Keras==2.0.4",
+    "pandas>=0.18.0",
+    "scikit-learn==0.18.1",
+    "scipy>=0.17.0",
+    "Theano==0.9.0",
+    "xgboost==0.6a2",
+    "lightgbm==2.0.2"
+]
 
 class OverrideInstallCommand(install):
     def run(self):
         # Install all requirements
         failed = []
 
-        for req in reqs:
+        for req in requirements:
             if pip.main(["install", req]) == 1:
                 failed.append(req)
 
@@ -24,50 +34,20 @@ class OverrideInstallCommand(install):
             print(str(failed))
             print("Please install them manually")
             print("")
-            return 1
+            raise OSError("Aborting")
 
         # install MlBox
         install.run(self)
 
-with open('README.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    "numpy>=1.13.0",
-    "matplotlib>=2.0.2",
-    "hyperopt==0.1",
-    "ipyparallel==6.0.0",
-    "Keras==2.0.4",
-    "pandas>=0.18.0",
-    "scikit-learn>=0.18.1",
-    "scipy>=0.17.0",
-    "Theano==0.9.0",
-    "xgboost==0.6a2",
-    "lightgbm==2.0.2"
-]
-
-test_requirements = [
-    "numpy>=1.13.0",
-    "matplotlib>=2.0.2",
-    "hyperopt==0.1",
-    "ipyparallel==6.0.0",
-    "Keras==2.0.4",
-    "pandas>=0.18.0",
-    "scikit-learn>=0.18.1",
-    "scipy>=0.17.0",
-    "Theano==0.9.0",
-    "xgboost==0.6a2",
-    "lightgbm==2.0.2"
-]
 
 setup(
     name='mlbox',
-    version='0.2.3-dev',
-    description="A powerful Automated Machine Learning python library  ",
-    long_description=readme + '\n\n' + history,
+    version='0.3.1',
+    description="A powerful Automated Machine Learning python library.",
+    long_description=readme,
     author="Axel ARONIO DE ROMBLAY",
     author_email='axelderomblay@gmail.com',
     url='https://github.com/AxeldeRomblay/mlbox',
@@ -91,20 +71,32 @@ setup(
     cmdclass={'install': OverrideInstallCommand},
     install_requires=requirements,
     zip_safe=False,
-    keywords='mlbox',
+    license='BSD-3',
+    keywords='mlbox auto-ml stacking pipeline optimisation',
     classifiers=[
-        'Development Status :: 3 - Pre-Alpha',
+        
+        'Development Status :: 5 - Production/Stable',
+        
         'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        
+        'License :: OSI Approved :: BSD License',
+        
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
+        
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=requirements
 )
